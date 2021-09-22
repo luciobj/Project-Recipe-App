634848e4730b89a5f58dd3832a441e8ab2fcef3a
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-09-22 17:18:30 -0300

    implementando testes requisito 14/15

--- src/tests/MealsPage.test.js
+++ src/tests/MealsPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../utils/renderWithRouter';
 
@@ -44,13 +44,62 @@ describe('Requisito 14 - radio buttons para filtragem', () => {
     expect(headerSearchButton).toBeInTheDocument();
   });
   it('Verifica se a busca por INGREDIENTE é feita corretamente', () => {
+    const drinks = {
+      idDrink: '12710',
+      strDrink: 'Apple Berry Smoothie',
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xwqvur1468876473.jpg',
+    };
+
+    global.fetch = jest.fn(async () => ({
+      json:async () => drinks,
+    }));
+
     const { history } = renderWithRouter(<App />);
     history.push('/comidas');
 
     const searchInput = screen.getByTestId('search-input');
     const ingredientRadioInput = screen.getByText('Ingrediente');
+    const searchButton = screen.getByText('Buscar');
 
     expect(searchInput).toBeInTheDocument();
+    expect(searchInput).toHaveValue('');
     expect(ingredientRadioInput).toBeInTheDocument();
+    expect(searchButton).toBeInTheDocument();
+
+    fireEvent.change(searchInput, { target: { value: 'a' } });
+    expect(searchInput).toHaveValue('a');
+    fireEvent.click(searchButton);
+
+    expect(global.fetch).toBeCalledTimes(1);
   });
+});
+
+it('Verifica se a busca por NOME é feita corretamente', () => {
+  const drinks = {
+    idDrink: '12710',
+    strDrink: 'Apple Berry Smoothie',
+    strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/xwqvur1468876473.jpg',
+  };
+
+  global.fetch = jest.fn(async () => ({
+    json: async () => drinks,
+  }));
+
+  const { history } = renderWithRouter(<App />);
+  history.push('/comidas');
+
+  const searchInput = screen.getByTestId('search-input');
+  const ingredientRadioInput = screen.getByText('Ingrediente');
+  const searchButton = screen.getByText('Buscar');
+
+  expect(searchInput).toBeInTheDocument();
+  expect(searchInput).toHaveValue('');
+  expect(ingredientRadioInput).toBeInTheDocument();
+  expect(searchButton).toBeInTheDocument();
+
+  fireEvent.change(searchInput, { target: { value: 'a' } });
+  expect(searchInput).toHaveValue('a');
+  fireEvent.click(searchButton);
+
+  expect(global.fetch).toBeCalledTimes(1);
 });
