2928607270bd27864566d94900794b958c418d4c
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-09-23 19:17:59 -0300

    Resolvendo erros de lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
