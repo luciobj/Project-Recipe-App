94efd199df78d6c3052964f7466b146f7857ef28
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-09-24 16:14:26 -0300

    Resolvendo conflitos do package

--- src/App.js
+++ src/App.js
@@ -1,53 +1,47 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
-import LoginProvider from './utils/LoginProvider';
-import Profile from './pages/Profile';
-import Explore from './pages/Explore';
-import Meals from './pages/Meals';
-import Drinks from './pages/Drinks';
-import ExploreMeals from './pages/ExploreMeals';
-import ExploreDrinks from './pages/ExploreDrinks';
-import IngredientsExplore from './pages/IngredientsExplore';
-import OriginExplore from './pages/OriginExplore';
-import RecipesMade from './pages/RecipesMade';
-import FavoriteRecipes from './pages/FavoriteRecipes';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./pages/Login";
+import LoginProvider from "./utils/LoginProvider";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import Meals from "./pages/Meals";
+import Drinks from "./pages/Drinks";
+import ExploreMeals from "./pages/ExploreMeals";
+import ExploreDrinks from "./pages/ExploreDrinks";
+import IngredientsExplore from "./pages/IngredientsExplore";
+import OriginExplore from "./pages/OriginExplore";
+import RecipesMade from "./pages/RecipesMade";
+import FavoriteRecipes from "./pages/FavoriteRecipes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Switch>
       <LoginProvider>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/perfil" component={ Profile } />
-          <Route exact path="/receitas-feitas" component={ RecipesMade } />
-          <Route exact path="/receitas-favoritas" component={ FavoriteRecipes } />
-          <Route exact path="/comidas" component={ Meals } />
-          <Route exact path="/bebidas" component={ Drinks } />
-          <Route exact path="/explorar" component={ Explore } />
-          <Route exact path="/explorar/comidas" component={ ExploreMeals } />
-          <Route
-            exact
-            path="/explorar/comidas/ingredientes"
-            component={ IngredientsExplore }
-          />
-          <Route
-            exact
-            path="/explorar/bebidas/ingredientes"
-            component={ IngredientsExplore }
-          />
-          <Route
-            exact
-            path="/explorar/comidas/area"
-            component={ OriginExplore }
-          />
-          <Route exact path="/explorar/bebidas" component={ ExploreDrinks } />
-        </Switch>
+        <Route exact path="/" component={Login} />
       </LoginProvider>
-    </BrowserRouter>
+      <Route path="/perfil" component={Profile} />
+      <Route exact path="/receitas-feitas" component={RecipesMade} />
+      <Route exact path="/receitas-favoritas" component={FavoriteRecipes} />
+      <Route exact path="/comidas" component={Meals} />
+      <Route exact path="/bebidas" component={Drinks} />
+      <Route exact path="/explorar" component={Explore} />
+      <Route exact path="/explorar/comidas" component={ExploreMeals} />
+      <Route
+        exact
+        path="/explorar/comidas/ingredientes"
+        component={IngredientsExplore}
+      />
+      <Route
+        exact
+        path="/explorar/bebidas/ingredientes"
+        component={IngredientsExplore}
+      />
+      <Route exact path="/explorar/comidas/area" component={OriginExplore} />
+      <Route exact path="/explorar/bebidas" component={ExploreDrinks} />
+    </Switch>
   );
 }
 
