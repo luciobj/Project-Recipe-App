2e13b1d2adc44474049002c521e5d003e2514726
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-09-27 19:09:15 -0300

    Alteraçao na resoluçao do retorno da API em caso de erro (randomRecipesAPI)

--- src/Services/randomRecipeAPI.js
+++ src/Services/randomRecipeAPI.js
@@ -1,13 +1,14 @@
 const RANDOM_MEAL_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 const RANDOM_DRINK_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const ERROR_MESSAGE = 'Não foi possível realizar pesquisa';
 
 export const fetchRandomMeal = async () => {
   try {
     const response = await fetch(RANDOM_MEAL_URL);
     const { meals } = await response.json();
     return meals;
-  } catch (error) {
-    throw new Error('Não foi possível realizar pesquisa');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
   }
 };
 
@@ -16,7 +17,7 @@ export const fetchRandomDrink = async () => {
     const response = await fetch(RANDOM_DRINK_URL);
     const { drinks } = await response.json();
     return drinks;
-  } catch (error) {
-    throw new Error('Não foi possível realizar pesquisa');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
   }
 };
