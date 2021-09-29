cfc37bda666677cdd66e25eb8782aeafd9ce0ce5
Lúcio BJ <lbjunq@hotmail.com>
2021-09-29 19:38:59 -0300

    Correcao de lint

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -56,23 +56,24 @@ export default function Categories(props) {
   return (
     <div>
       { loading ? <p>Carregando categorias</p>
-        : <label htmlFor="categories">
-          { categories && categories
-            .map((category) => (
-              <label htmlFor={ category.strCategory } key={ category.strCategory }>
-                { category.strCategory }
-                <input
-                  name="categories"
-                  id={ category.strCategory }
-                  type="radio"
-                  data-testid={ `${category.strCategory}-category-filter` }
-                  value={ category.strCategory }
-                  onClick={ handleClick }
-                />
-              </label>
-            )).slice(0, maxRender) }
-        </label>
-      }
+        : (
+          <label htmlFor="categories">
+            { categories && categories
+              .map((category) => (
+                <label htmlFor={ category.strCategory } key={ category.strCategory }>
+                  { category.strCategory }
+                  <input
+                    name="categories"
+                    id={ category.strCategory }
+                    type="radio"
+                    data-testid={ `${category.strCategory}-category-filter` }
+                    value={ category.strCategory }
+                    onClick={ handleClick }
+                  />
+                </label>
+              )).slice(0, maxRender) }
+          </label>
+        ) }
     </div>
   );
 }
