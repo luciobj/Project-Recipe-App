5e3f0ef5c17d2e08cb14f540ae8c40783155714d
Itaopara Neto <itaopara@gmail.com>
2021-09-28 21:44:20 -0300

    Requisitos 75 e 76

--- src/pages/IngredientsExplore.js
+++ src/pages/IngredientsExplore.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
-import IngredientsCards from '../Components/IngredientsCards';
+import IngredientsMealsCards from '../Components/IngredientsMealsCards';
+import IngredientsDrinksCards from '../Components/IngredientsDrinksCards';
 
+const URL = window.location.pathname;
 function IngredientsExplore() {
   return (
     <div>
       <Header title="Explorar Ingredientes" containBtnSearch />
-      <IngredientsCards />
+      {URL === '/explorar/bebidas/ingredientes'
+        ? <IngredientsDrinksCards />
+        : <IngredientsMealsCards />}
       <Footer />
     </div>
   );
