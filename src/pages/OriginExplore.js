77be462552fb91da989d131006d1bec805f7006b
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-10-05 19:00:15 -0300

    Implementa tela de explorar por origem

--- src/pages/OriginExplore.js
+++ src/pages/OriginExplore.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import RecipesProvider from '../context/recipesProvider';
+import ExploreArea from '../Components/ExploreArea';
 
 const OriginExplore = () => (
   <RecipesProvider>
     <div>
       <Header title="Explorar Origem" />
+      <ExploreArea />
       <Footer />
     </div>
   </RecipesProvider>
